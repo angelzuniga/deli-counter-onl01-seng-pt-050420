f181d11ccb997c53c967dce8a96958007fc41602
Angel Zuniga <angelzuniga@outlook.com>
2020-05-16 23:51:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,11 @@
-katz_deli = []
-
-def line(katz_deli)
-  if katz_deli.empty?
+def line(deli)
+  if deli.empty?
     puts "The line is currently empty."
   else
     current_line = "The line is currently:"
     deli.each.with_index(1) do |person, i|
       current_line << " #{i}. #{person}"
     end
-  end
     puts current_line
-
-
+  end
+end
